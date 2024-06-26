278357c54c85f968fcf16a6e53740db3063d546c
Sahan Maduwantha <125999503+MrMasterOfc@users.noreply.github.com>
2024-06-26 03:15:16 -0700

    👨‍💻ＭＡＳＴＥＲ－ＭＩＮＤ👨‍💻

---
+++ script.js
@@ -0,0 +1,17 @@
+const header = document.querySelector("header");
+
+window.addEventListener("scroll", function() {
+    header.classList.toggle("sticky",this.window.scrollY > 120);
+})
+let menu = document.querySelector("#menu-icon");
+let navlist = document.querySelector('.navlist');
+
+menu.onclick = () =>{
+    menu.classList.toggle('bx-x');
+    navlist.classList.toggle('active');
+}
+
+window.onscroll = () =>{
+    menu.remove.toggle('bx-x');
+    navlist.remove.toggle('active');
+}
